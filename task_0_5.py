9b5bb2effb27461e37a615e75b5d3d96def35087
Kholofelo Moropane <rkmoropane@gmail.com>
2023-04-22 18:54:03 +0200

    Done, added the task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,4 +1,4 @@
-def area_of_triangle(side_1: int, side_2: int, side_3: int):
+def area_of_triangle(side_1, side_2, side_3):
     semi = 1 / 2 * (side_1 + side_2 + side_3)
     total_area = (semi * (semi - side_1) * (semi - side_2) * (semi - side_3)) ** (1 / 2)
     return total_area
