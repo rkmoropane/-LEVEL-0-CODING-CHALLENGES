9bf93fa1a763a86cee434c324690bfb4cb50bd51
Kholofelo Moropane <rkmoropane@gmail.com>
2023-04-22 14:43:51 +0200

    Modified the task_0_6.py

--- task_0_6.py
+++ task_0_6.py
@@ -1,6 +1,6 @@
 def maximum(*numbers: int):
     max_number = numbers[0]
-    for number in numbers:
+    for number in numbers[1:]:
         if number > max_number:
             max_number = number
     return max_number
