9d50374d012bdc83d212fa6cdd7aecc36a5bd5ff
Kholofelo Moropane <rkmoropane@gmail.com>
2023-04-22 18:15:03 +0200

    Done, added the task_0_10.py

---
+++ task_0_10.py
@@ -0,0 +1,15 @@
+def find_common_letters(string_1, string_2):
+    string_1 = string_1.lower()
+    string_2 = string_2.lower()
+    delimeter = ", "
+    common_letters = []
+
+    for letter in string_2:
+        for character in string_1:
+            if letter == character and letter not in common_letters:
+                common_letters.append(letter)
+    common_letters = delimeter.join(common_letters)
+    print(f"Common letters: {common_letters}")
+
+
+find_common_letters("house", "computers")
