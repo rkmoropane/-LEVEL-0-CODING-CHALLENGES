0d9e70d0e4b490e7515c4e482c87024f8733191f
Kholofelo Moropane <rkmoropane@gmail.com>
2023-04-22 18:11:35 +0200

    Done, added the task_0_7.py

---
+++ task_0_7.py
@@ -0,0 +1,12 @@
+def celsius_to_fahrenheit(temperature):
+    fahrenheit = temperature * (9 / 5) + 32
+    return fahrenheit
+
+
+def fahrenheit_to_celsius(temperature):
+    celsius = (temperature - 32) * (5 / 9)
+    return celsius
+
+
+print(celsius_to_fahrenheit(37))
+print(fahrenheit_to_celsius(90))
