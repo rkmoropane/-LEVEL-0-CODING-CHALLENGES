9f7b80445f4281dad5ea9c08d859d8995d4841ab
Kholofelo Moropane <rkmoropane@gmail.com>
2023-04-26 19:34:07 +0200

    Done, added the task_0_8.py.

--- task_0_8.py
+++ task_0_8.py
@@ -5,10 +5,10 @@ def convert_minutes_to_hours_and_minutes(number):
     minutes_string = "minutes" if min != 1 else "minute"
 
     result = f"{hours} {hour_string}, {min} {minutes_string}"
-    return result
+    print(result)
 
 
 if __name__ == "__main__":
-    print(convert_minutes_to_hours_and_minutes(71))
-    print(convert_minutes_to_hours_and_minutes(133))
-    print(convert_minutes_to_hours_and_minutes(30))
+    convert_minutes_to_hours_and_minutes(71)
+    convert_minutes_to_hours_and_minutes(133)
+    convert_minutes_to_hours_and_minutes(30)
