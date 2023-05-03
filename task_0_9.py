95532d77a6ce89271ab22d3e649765eef77fb7d9
Kholofelo Moropane <rkmoropane@gmail.com>
2023-05-03 09:19:19 +0200

    Done, fixed task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -1,14 +1,12 @@
 def find_vowels(string):
     vowels_found = []
-    delimeter = ", "
     string = string.lower()
     vowels = "aeiou"
     for letter in string:
-        if letter in vowels:
-            if letter not in vowels_found:
+        if letter in vowels and letter not in vowels_found:
                 vowels_found.append(letter)
 
-    vowels_found = delimeter.join(vowels_found)
+    vowels_found = ", ".join(vowels_found)
     print(f"Vowels: {vowels_found}")
 
 
