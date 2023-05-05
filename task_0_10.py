b56d974eeced658c027aa5ee4a951e146e11dd02
Kholofelo Moropane <rkmoropane@gmail.com>
2023-05-05 08:39:34 +0200

    Done, fixed task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,14 +1,14 @@
 def find_common_letters(string_1, string_2):
     string_1 = string_1.lower()
     string_2 = string_2.lower()
-    
+
     common_letters = []
-    for letter in string_2: 
+    for letter in string_2:
         if letter in string_1 and letter not in common_letters:
             common_letters.append(letter)
 
-    common_letters = ", ".join(common_letters)
-    print(f"Common letters: {common_letters}")
+    common_letters_string = ", ".join(common_letters)
+    print(f"Common letters: {common_letters_string}")
 
 
 if __name__ == "__main__":
